8a0a6e5b87a1b1794c0469c4588aa81081d47489
Sarfaraz <ansarisarfarazlive.com>
2020-12-02 17:26:07 +0530

    make it better

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const routes = require('./routes/routes');
 
 const app = express();
